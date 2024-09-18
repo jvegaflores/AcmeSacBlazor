e3ff075fbf754e994de441f820fa34057b76d2db
Jorge Alberto Vega Flores <jvega_flores@hotmail.com>
2024-09-18 16:39:52 -0500

    #Implementando reposoitorio

---
+++ AcmeSacBlazor.Web/Repositorio/CategoriaRepositorio.cs
@@ -0,0 +1,39 @@
+﻿using AcmeSacBlazor.Web.Data;
+using AcmeSacBlazor.Web.Repositorio.IRepositorio;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcmeSacBlazor.Web.Repositorio
+{
+    public class CategoriaRepositorio : ICategoriaRepositorio
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Categoria Agregar(Categoria categoria)
+        {
+            _context.categorias.Add(categoria);
+            _context.SaveChanges();
+            return categoria;
+        }
+
+        public IEnumerable<Categoria> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Categoria GetOne(int Id)
+        {
+            var BuscaCategoria = _context.categorias.FirstOrDefault(c => c.Id == Id);
+
+            if (BuscaCategoria == null)
+            {
+                return new Categoria();
+            }
+            return BuscaCategoria;
+        }
+
+        public Categoria Modificar(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AcmeSacBlazor.Web/Repositorio/IRepositorio/ICategoriaRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using AcmeSacBlazor.Web.Data;
+
+namespace AcmeSacBlazor.Web.Repositorio.IRepositorio
+{
+    public interface ICategoriaRepositorio
+    {
+        public Categoria Agregar(Categoria categoria);
+        public Categoria Modificar(Categoria categoria);
+        public Categoria GetOne(int Id);
+        public IEnumerable<Categoria> GetAll();
+    }
+}
